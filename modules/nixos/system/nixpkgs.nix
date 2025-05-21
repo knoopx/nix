5caa4275db3b67e27f8ebb8b763aff77a8c48371
Victor Martinez <knoopx@gmail.com>
2025-05-21 15:28:31 +0200

    remove deprecated insecure packages from nixpkgs configuration

--- modules/nixos/system/nixpkgs.nix
+++ modules/nixos/system/nixpkgs.nix
@@ -5,8 +5,6 @@ _: {
       allowUnfree = true;
       allowUnfreePredicate = true;
       permittedInsecurePackages = [
-        "freeimage-unstable-2021-11-01"
-        "SDL_ttf-2.0.11"
       ];
     };
   };
