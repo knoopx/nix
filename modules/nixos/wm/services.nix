88bd1ed4f5283695a56de488ab0d8a7337aa2370
Victor Martinez <knoopx@gmail.com>
2025-05-27 20:55:19 +0200

    use gdm for now

--- modules/nixos/wm/services.nix
+++ modules/nixos/wm/services.nix
@@ -1,9 +1,21 @@
 {pkgs, ...}: {
+  security.pam.services.greetd = {
+    enableGnomeKeyring = true;
+  };
+  services.xserver.displayManager.gdm.enable = true;
+
   services = {
+    # xserver.displayManager.lightdm = {
+    # enable = true;
+    # greeter.enable = false;
+    # greeters.slick.enable = true;
+    # };
+
     displayManager = {
       sessionPackages = with pkgs; [
         niri
       ];
+      defaultSession = "niri";
     };
     gnome = {
       gnome-keyring.enable = true;
