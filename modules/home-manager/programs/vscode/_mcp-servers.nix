474b9d88c256f221c3bb6a29de1c8ba269c2fa5c
Victor Martinez <knoopx@gmail.com>
2025-06-03 15:47:54 +0200

    feat: simplify mcp-servers configuration by removing commented-out server entries

--- modules/home-manager/programs/vscode/_mcp-servers.nix
+++ modules/home-manager/programs/vscode/_mcp-servers.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{pkgs, ...}: {
   console-ninja = {
     command = "npx";
     args = [
@@ -11,83 +7,14 @@
       "node ~/.console-ninja/mcp/"
     ];
   };
+
+  context7 = {
+    type = "http";
+    url = "https://mcp.context7.com/mcp";
+  };
+
   puppeteer = {
     command = "${pkgs.nodejs}/bin/npx";
     args = ["-y" "@modelcontextprotocol/server-puppeteer"];
   };
-
-  # memory = {
-  #   command = "${pkgs.nodejs}/bin/npx";
-  #   args = ["-y" "@modelcontextprotocol/server-memory"];
-  # };
-
-  # time = {
-  #   command = "${pkgs.uv}/bin/uvx";
-  #   args = ["mcp-server-time" "--local-timezone=Europe/Madrid"];
-  # };
-  # fetch = {
-  #   command = "${pkgs.uv}/bin/uvx";
-  #   args = ["mcp-server-fetch"];
-  # };
-
-  # "postgres" = {
-  #   "command" = "${pkgs.nodejs}/bin/npx";
-  #   "args" = [
-  #     "-y"
-  #     "@modelcontextprotocol/server-postgres"
-  #     "postgresql://localhost/mydb"
-  #   ];
-  # };
-  # playwright = {
-  #   command = "${pkgs.nodejs}/bin/npx";
-  #   args = [
-  #     "@playwright/mcp@latest"
-  #     # "--browser firefox"
-  #     # "--headless"
-  #     # "--vision"
-  #   ];
-  # };
-  # documents = {
-  #   command = "${pkgs.nodejs}/bin/npx";
-  #   args = ["-y" "@modelcontextprotocol/server-filesystem" "${config.home.homeDirectory}/Documents"];
-  # };
-
-  # "github": {
-  #   "command": "/bin/sh",
-  #   "args":  ["-c", "PATH=/run/current-system/sw/bin:$PATH exec npx -y @modelcontextprotocol/server-github ${documents_dir}"],
-  #   "env": {
-  #     "GITHUB_PERSONAL_ACCESS_TOKEN": "${githubToken}"
-  #   }
-  # },
-  # "git": {
-  #   "command": "/bin/sh",
-  #   "args": ["-c", "PATH=/run/current-system/sw/bin:$PATH exec uvx mcp-server-git --repository ${documents_dir}"]
-  # }
-
-  # servers.sitemcp-react = {
-  #   command = "${pkgs.nodePackages.sitemcp}/bin/sitemcp";
-  #   args = [
-  #     "https://react.dev/reference/react"
-  #     "--concurrency"
-  #     "10"
-  #   ];
-  # };
-
-  # servers.sitemcp-tailwindcss = {
-  #   command = "${pkgs.nodePackages.sitemcp}/bin/sitemcp";
-  #   args = [
-  #     "https://tailwindcss.com/docs/"
-  #     "--concurrency"
-  #     "10"
-  #   ];
-  # };
-
-  # servers.sitemcp-pytorch = {
-  #   command = "${pkgs.nodePackages.sitemcp}/bin/sitemcp";
-  #   args = [
-  #     "https://pytorch.org/docs/stable/index.html"
-  #     "--concurrency"
-  #     "10"
-  #   ];
-  # };
 }
