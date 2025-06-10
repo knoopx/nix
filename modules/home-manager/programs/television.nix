c0c9cfae05e2be4eef7fbb563d3210f91d897be2
Victor Martinez <knoopx@gmail.com>
2025-06-10 09:58:32 +0200

    fix: update file paths in television configuration to use xdg.configFile

--- modules/home-manager/programs/television.nix
+++ modules/home-manager/programs/television.nix
@@ -1,5 +1,5 @@
 {config, ...}: {
-  home.file."${config.xdg.configHome}/television/config.toml".text = ''
+  xdg.configFile."/television/config.toml".text = ''
     [ui]
     use_nerd_font_icons = true
     input_bar_position = "top"
@@ -9,7 +9,7 @@
     theme = "${config.programs.bat.config.theme}"
   '';
 
-  home.file."${config.xdg.configHome}/television/channels.toml".text = ''
+  xdg.configFile."/television/channels.toml".text = ''
     [[cable_channel]]
     name = "git-diff"
     source_command = "git diff --name-only"
