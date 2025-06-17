ec27005669160754214516661579ab38302152ba
Victor Martinez <knoopx@gmail.com>
2025-06-17 20:33:39 +0200

    feat(discourse): ✨ Add Discord user style
    Introduced a new user style for Discord using the Catppuccin theme. This allows users to customize their Discord interface with a new aesthetic.
    
    - Fetches the Catppuccin Mocha theme CSS from the specified URL.
    - Writes the user style to a new file for use in Discord.
    
    This enhances the theming options available for users.

---
+++ builders/theming/userstyles/discord.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  userstyle = pkgs.fetchurl {
+    url = "https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css";
+    sha256 = "sha256-SXFiwmFFOuBR5SEr+Ddfv3IK4dlhKHdwHMkbd4dPiaE=";
+  };
+in
+  pkgs.writeTextFile {
+    name = "telegram.userstyle.css";
+    text = ''
+      @-moz-document domain("discord.com") {
+        ${lib.readFile userstyle}
+      };
+    '';
+  }
