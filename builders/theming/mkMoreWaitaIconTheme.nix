9af54e7d0c2536fc7717512032574b8c9cbb4c66
Victor Martinez <knoopx@gmail.com>
2025-06-17 23:32:46 +0200

    feat(theming): ✨ Enhance MoreWaita icon theme build process
    Improved the build process for the MoreWaita icon theme by adding native build inputs for SVGO and Lutgen. This allows for optimized SVG files and better integration of the color scheme during installation.
    
    - Added SVGO for SVG optimization.
    - Included Lutgen for applying color schemes to SVG files.

--- builders/theming/mkMoreWaitaIconTheme.nix
+++ builders/theming/mkMoreWaitaIconTheme.nix
@@ -1,10 +1,15 @@
 {
   pkgs,
+  lib,
   adwaita-colors,
   ...
 }: colorScheme:
 pkgs.morewaita-icon-theme.overrideAttrs (prev: {
+  nativeBuildInputs = (prev.nativeBuildInputs or []) ++ [pkgs.nodePackages.svgo pkgs.lutgen];
+
   postInstall = ''
     cp -r ${adwaita-colors}/Adwaita-blue/* $out/share/icons/MoreWaita
+    find $out/share/icons/MoreWaita -name "*.svg" -type f -print0 | xargs -0 -n1 -P$(nproc) sh -c 'svgo "$1" || true' _
+    find $out -name "*.svg" -type f -print0 | xargs -0 -n1 -P$(nproc) sh -c 'lutgen patch --write "$1" -- ${lib.concatStringsSep " " (lib.attrValues colorScheme)} || true' _
   '';
 })
