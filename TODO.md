ccede4ea560e8ae1b063b4406aee37a5c0586c9d
Victor Martinez <knoopx@gmail.com>
2025-06-19 10:27:33 +0200

    add(todos): ➕ Add TODO list for Minibook X
    Created a new TODO file to track enhancements and issues related to the Minibook X. This includes various tasks such as swapping keys, implementing tablet mode, and addressing battery notifications.
    
    - Added a comprehensive list of tasks to improve functionality.
    - Included links to relevant issues for reference.

---
+++ TODO.md
@@ -0,0 +1,17 @@
+# TODO
+
+- gparted not launching (polkit issues)
+
+## Minibook X
+
+- swap alt<->window keys (kmonad/keyd)
+- tablet-mode
+- home encryption
+- proper locking
+- energy saving
+- charge limit 80%
+- 60hz edid
+- low-batery/charging notifications
+- battery meter not updating
+- https://github.com/petitstrawberry/minibook-support/issues/17
+- https://github.com/arkaitzsilva/dotfiles/blob/b58798fd6e6cfeccfc3148c444a94ac00bca2cd2/hosts/Alienware13/dsdt-override.nix
--- hosts/minibookx/default.nix
+++ hosts/minibookx/default.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  config,
-  ...
-} @ inputs: let
+{config, ...} @ inputs: let
   listNixModulesRecusive = import ../../lib/listNixModulesRecusive.nix inputs;
   system = "x86_64-linux";
 in {
@@ -46,20 +42,5 @@ in {
   defaults.display.defaultColumnWidthPercent = 1.0;
   defaults.display.columnWidthPercentPresets = [0.5 0.75];
 
-  # TODOS
-
-  # swap alt<->window keys (kmonad/keyd)
-  # tablet-mode
-  # home encryption
-  # proper locking
-  # energy saving
-  # charge limit 80%
-  # 60hz edid
-  # low-batery/charging notifications
-  # battery meter not updating
-
   home-manager.users.${config.defaults.username} = import ../../home/${config.defaults.username}.nix;
-
-  # https://github.com/petitstrawberry/minibook-support/issues/17
-  # https://github.com/arkaitzsilva/dotfiles/blob/b58798fd6e6cfeccfc3148c444a94ac00bca2cd2/hosts/Alienware13/dsdt-override.nix#L5
 }
