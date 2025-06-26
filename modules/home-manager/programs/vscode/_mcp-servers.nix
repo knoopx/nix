164b757e314c203f43b0d247e18cd92396ef1dfa
Victor Martinez <knoopx@gmail.com>
2025-06-27 01:26:32 +0200

    feat(mcp-servers): ✨ Add new MCP server configurations
    Introduced new configurations for various MCP servers including deepwiki, huggingface, markitdown, and github. This enhances the functionality and usability of the MCP server setup.
    
    - Added configurations for deepwiki and huggingface with appropriate commands.
    - Included markitdown and github server configurations with necessary environment variables.
    
    This update aims to streamline the setup process for developers using these services.

--- modules/home-manager/programs/vscode/_mcp-servers.nix
+++ modules/home-manager/programs/vscode/_mcp-servers.nix
@@ -13,8 +13,54 @@
     url = "https://mcp.context7.com/mcp";
   };
 
-  puppeteer = {
-    command = "${pkgs.nodejs}/bin/npx";
-    args = ["-y" "@modelcontextprotocol/server-puppeteer"];
+  "playwright" = {
+    "command" = "bunx";
+    "args" = [" @playwright/mcp@latest"];
   };
+
+  "deepwiki" = {
+    "command" = "bunx";
+    "args" = ["mcp-deepwiki"];
+    "trust" = true;
+  };
+
+  "huggingface" = {
+    "command" = "uvx";
+    "args" = ["huggingface-mcp-server"];
+    "trust" = true;
+  };
+
+  "markitdown" = {
+    "command" = "uvx";
+    "args" = ["markitdown-mcp"];
+    "trust" = true;
+  };
+
+  "github" = {
+    "command" = "docker";
+    "args" = [
+      "run"
+      "-i"
+      "--rm"
+      "-e"
+      "GITHUB_PERSONAL_ACCESS_TOKEN"
+      "-v"
+      "$'{PWD}:/workspace"
+      "ghcr.io/github/github-mcp-server"
+    ];
+    "env" = {
+      "GITHUB_PERSONAL_ACCESS_TOKEN" = "$'{GH_TOKEN}";
+    };
+  };
+
+  # puppeteer = {
+  #   command = "${pkgs.nodejs}/bin/npx";
+  #   args = ["-y" "@modelcontextprotocol/server-puppeteer"];
+  # };
+
+  # "duckdb" = {
+  #   "command" = "uvx";
+  #   "args" = ["mcp-server-duckdb"];
+  #   "trust" = true;
+  # };
 }
