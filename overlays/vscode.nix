b6e447647bc88c8d8fe01af0dfc5fe3aa3993be7
Victor Martinez <knoopx@gmail.com>
2025-07-11 00:22:13 +0200

    fix(vscode): 🐛 Update VSCode version to 1.102.0
    Changed the VSCode version from 'latest' to '1.102.0' and updated the source URL to point to the stable release. This ensures that the build uses a specific version, improving consistency and reliability.
    
    - Updated version to 1.102.0.
    - Changed source URL to the stable release.

--- overlays/vscode.nix
+++ overlays/vscode.nix
@@ -1,15 +1,10 @@
 final: prev: {
-  vscode =
-    (prev.vscode.overrideAttrs (oldAttrs: {
-      version = "latest";
-      src = builtins.fetchTarball {
-        url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
-        sha256 = "sha256:099vnvh9j163n9yfsqiasy2aq6jhh77ls964017mph1a84njmz7v";
-      };
-      postInstall = ''
-        ln -s $out/bin/code-insiders $out/bin/code
-      '';
-    })).override {
-      isInsiders = true;
+  vscode = prev.vscode.overrideAttrs (oldAttrs: rec {
+    version = "1.102.0";
+    src = prev.fetchurl {
+      name = "vscode-linux-x64-${version}.tar.gz";
+      url = "https://update.code.visualstudio.com/1.102.0/linux-x64/stable";
+      sha256 = "sha256-zgrNohvsmhcRQmkX7Io2/U3qbVWdcqwT7VK7Y3ENb9g=";
     };
+  });
 }
