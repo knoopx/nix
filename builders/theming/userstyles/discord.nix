55d8e7e6a71d257442b6240eaaebec6db6c8487c
Victor Martinez <knoopx@gmail.com>
2025-07-13 09:16:41 +0200

    fix(discord): 🐛 Update userstyle SHA256 hash
    Updated the SHA256 hash for the Discord userstyle to ensure the correct file is fetched. This change is necessary to maintain the integrity and security of the userstyle being applied.
    
    No functional changes were made to the userstyle itself.

--- builders/theming/userstyles/discord.nix
+++ builders/theming/userstyles/discord.nix
@@ -5,7 +5,7 @@
 }: let
   userstyle = pkgs.fetchurl {
     url = "https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css";
-    sha256 = "sha256-SXFiwmFFOuBR5SEr+Ddfv3IK4dlhKHdwHMkbd4dPiaE=";
+    sha256 = "sha256-1NIL4TbkHBZirwc4Uj+N+98I+a3B0KltfO6WQ1No9QQ=";
   };
 in
   pkgs.writeTextFile {
