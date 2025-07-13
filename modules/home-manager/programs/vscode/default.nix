78b9a8a99ada163455fd718545ac06e016ad7cae
Victor Martinez <knoopx@gmail.com>
2025-07-13 10:25:01 +0200

    fix(vscode): 🐛 Update VSCode configuration for MCP settings
    Refactored the VSCode configuration to streamline the management of MCP settings. This change consolidates the configuration for both the default and Insiders versions of VSCode, ensuring that the MCP settings are correctly applied.
    
    - Updated the xdg.configFile structure to use builtins.listToAttrs for better organization.
    - Included a new prompt file for the 4.1-Beast chat mode.
    
    This change improves maintainability and clarity in the configuration.

--- modules/home-manager/programs/vscode/default.nix
+++ modules/home-manager/programs/vscode/default.nix
@@ -5,9 +5,25 @@
 } @ inputs: {
   stylix.targets.vscode.profileNames = ["default"];
 
-  # xdg.configFile."Code/User/globalStorage/rooveterinaryinc.roo-cline/settings/mcp_settings.json".text = builtins.toJSON {servers = pkgs.callPackage ./_mcp-servers.nix inputs;};
-  xdg.configFile."Code/User/mcp.json".text = builtins.toJSON {servers = nixosConfig.ai.mcp;};
-  xdg.configFile."Code - Insiders/User/mcp.json".text = builtins.toJSON {servers = nixosConfig.ai.mcp;};
+  xdg.configFile = builtins.listToAttrs (builtins.concatMap (path: [
+    # {
+    #   name = "${path}/User/globalStorage/rooveterinaryinc.roo-cline/settings/mcp_settings.json";
+    #   value = { text = builtins.toJSON { servers = pkgs.callPackage ./_mcp-servers.nix inputs; }; };
+    # }
+    {
+      name = "${path}/User/mcp.json";
+      value = {text = builtins.toJSON {servers = nixosConfig.ai.mcp;};};
+    }
+    {
+      name = "${path}/User/prompts/4.1-Beast.chatmode.md";
+      value = {
+        source = pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/github/awesome-copilot/refs/heads/main/chatmodes/4.1-Beast.chatmode.md";
+          sha256 = "sha256-ISKHDpzTKN1h+MM0/E9f/wp6qfSSWp1rj9sqpVHKkR8=";
+        };
+      };
+    }
+  ]) ["Code" "Code - Insiders"]);
 
   programs = {
     vscode = {
