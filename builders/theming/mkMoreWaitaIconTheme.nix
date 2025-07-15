80c86d40daad5485101864186921385907e518e9
Victor Martinez <knoopx@gmail.com>
2025-07-15 19:18:30 +0200

    refactor(nixos): ♻️ Remove unused nativeBuildInputs
    Removed the unused `nativeBuildInputs` for `svgo` from the MoreWaita icon theme configuration. This simplifies the build process and reduces unnecessary dependencies.

--- builders/theming/mkMoreWaitaIconTheme.nix
+++ builders/theming/mkMoreWaitaIconTheme.nix
@@ -4,9 +4,7 @@
   ...
 }: colorScheme:
 pkgs.morewaita-icon-theme.overrideAttrs (prev: {
-  nativeBuildInputs = (prev.nativeBuildInputs or []) ++ [pkgs.nodePackages.svgo];
   postInstall = ''
     cp -r ${adwaita-colors}/Adwaita-blue/* $out/share/icons/MoreWaita
-    find $out/share/icons/MoreWaita -name "*.svg" -type f -print0 | xargs -0 -n1 -P$(nproc) sh -c 'svgo "$1" || true' _
   '';
 })
