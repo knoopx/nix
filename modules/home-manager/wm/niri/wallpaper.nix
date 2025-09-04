34794c89aa4a0a609f2edb43cf91e8de2e82859f
Victor Martinez <knoopx@gmail.com>
2025-09-05 00:24:34 +0200

    style(wallpaper): 💄 Update wallpaper pattern style
    Changed the wallpaper pattern style to 'zebra' for better visual appeal. This update enhances the overall aesthetic of the user interface by providing a more dynamic background.
    
    - Updated the style to use 'pkgs.pattern-monster.zebra'.
    - Commented out previous styles for clarity.

--- modules/home-manager/wm/niri/wallpaper.nix
+++ modules/home-manager/wm/niri/wallpaper.nix
@@ -6,14 +6,18 @@
 }: let
   mkSVGPatternWallpaper = pkgs.callPackage ../../../../builders/theming/mkSVGPatternWallpaper.nix {inherit pkgs lib nixosConfig;};
   wallpaper = mkSVGPatternWallpaper {
-    style = pkgs.pattern-monster.zebra;
+    # style = pkgs.pattern-monster.japanese-pattern-2;
+    # style = pkgs.pattern-monster.sprinkles-1;
+    # style = pkgs.pattern-monster.plaid-pattern-2;
+    # style = pkgs.pattern-monster.triangles-21;
+    # style = pkgs.pattern-monster.interlocked-hexagons-2;
+    style = pkgs.pattern-monster.triangles-18;
+    # style = pkgs.pattern-monster.hexagon-7;
+
     scale = 4;
     colors = with nixosConfig.defaults.colorScheme.palette; [
-      base01
       base00
-      base02
-      base03
-      # base04
+      base01
     ];
   };
 in {
