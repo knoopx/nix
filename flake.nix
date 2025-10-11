6d2e364019fa43d1024fab96e95cd855bb63049e
Victor Martinez <knoopx@gmail.com>
2025-10-11 17:36:19 +0200

    update(flake): ⬆️ Update nixpkgs references
    Updated the nixpkgs references in flake.lock and flake.nix to ensure compatibility with the latest changes. This includes modifying the structure of the nixpkgs input in the nix-vscode-extensions and removing outdated references in the flake.lock file.

--- flake.nix
+++ flake.nix
@@ -55,6 +55,7 @@
     nix-chuwi-minibook-x.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    nix-vscode-extensions.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
