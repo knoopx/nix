8c587eddba3a7d2de9f08027a0fb212cb04eef67
Victor Martinez <knoopx@gmail.com>
2025-10-11 14:24:51 +0200

    update(geary): ⬆️ Update Geary version and SHA256 hash
    Updated the Geary package to the latest version by changing the revision and SHA256 hash. This ensures that the package is fetched correctly from the repository and maintains integrity.
    
    - Changed revision to "0547dc399a60d90b5dbb5ae3fa73f6e590fd98a2".
    - Updated SHA256 hash to "sha256-b8hA7GJjazAL0zvGWlOSwqldA3XfkwjyCYYcv6oHU84=".

--- overlays/geary.nix
+++ overlays/geary.nix
@@ -3,8 +3,8 @@ final: prev: {
     version = "git";
     src = prev.fetchgit {
       url = "https://gitlab.gnome.org/GNOME/geary.git";
-      rev = "a43a1a0275d5fdf7da0005427feb6614ca0de438";
-      sha256 = "sha256-4x4moUu+MtkBy+agXlLtclW6QCciMh0Y9H0pdUW+G68=";
+      rev = "0547dc399a60d90b5dbb5ae3fa73f6e590fd98a2";
+      sha256 = "sha256-b8hA7GJjazAL0zvGWlOSwqldA3XfkwjyCYYcv6oHU84=";
     };
 
     postPatch = ''
