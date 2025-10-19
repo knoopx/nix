b51663947d171df096e77f405eb1a5f31657bd1a
Victor Martinez <knoopx@gmail.com>
2025-10-19 18:18:08 +0200

    feat(vicinae): migrate theme

--- modules/home-manager/programs/vicinae.nix
+++ modules/home-manager/programs/vicinae.nix
@@ -1,30 +1,44 @@
 {
   config,
   nixosConfig,
+  lib,
+  pkgs,
   ...
-}: {
+}: let
+  customTheme = pkgs.writeText "custom-theme.toml" ''
+    [meta]
+    version = 1
+    name = "Custom Theme"
+    description = "Custom Theme"
+    variant = "dark"
+    inherits = "vicinae-dark"
+
+    [colors.core]
+    accent = "#${nixosConfig.defaults.colorScheme.palette.base0D}"
+    background = "#${nixosConfig.defaults.colorScheme.palette.base00}"
+    foreground = "#${nixosConfig.defaults.colorScheme.palette.base05}"
+    secondary_background = "#${nixosConfig.defaults.colorScheme.palette.base0D}"
+    border = "#${nixosConfig.defaults.colorScheme.palette.base02}"
+
+    [colors.accents]
+    blue = "#${nixosConfig.defaults.colorScheme.palette.base0D}"
+    green = "#${nixosConfig.defaults.colorScheme.palette.base0B}"
+    magenta = "#${nixosConfig.defaults.colorScheme.palette.base0F}"
+    orange = "#${nixosConfig.defaults.colorScheme.palette.base09}"
+    purple = "#${nixosConfig.defaults.colorScheme.palette.base0E}"
+    red = "#${nixosConfig.defaults.colorScheme.palette.base08}"
+    yellow = "#${nixosConfig.defaults.colorScheme.palette.base0A}"
+    cyan = "#${nixosConfig.defaults.colorScheme.palette.base0C}"
+  '';
+in {
   services.vicinae.enable = true;
 
+  home.activation.createVicinaeTheme = config.lib.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p ${config.home.homeDirectory}/.local/share/flatpak/exports/share/vicinae/themes
+    cp ${customTheme} ${config.home.homeDirectory}/.local/share/flatpak/exports/share/vicinae/themes/custom.toml
+  '';
+
   # https://docs.vicinae.com/theming#creating-a-custom-theme
-  home.file.".config/vicinae/themes/custom.json" = {
-    text = builtins.toJSON {
-      version = "1.0.0";
-      appearance = "dark";
-      name = "Custom";
-      palette = {
-        background = "#${nixosConfig.defaults.colorScheme.palette.base00}";
-        foreground = "#${nixosConfig.defaults.colorScheme.palette.base05}";
-        blue = "#${nixosConfig.defaults.colorScheme.palette.base0D}";
-        green = "#${nixosConfig.defaults.colorScheme.palette.base0B}";
-        magenta = "#${nixosConfig.defaults.colorScheme.palette.base0F}";
-        orange = "#${nixosConfig.defaults.colorScheme.palette.base09}";
-        purple = "#${nixosConfig.defaults.colorScheme.palette.base0E}";
-        red = "#${nixosConfig.defaults.colorScheme.palette.base08}";
-        yellow = "#${nixosConfig.defaults.colorScheme.palette.base0A}";
-        cyan = "#${nixosConfig.defaults.colorScheme.palette.base0C}";
-      };
-    };
-  };
 
   home.file.".config/vicinae/vicinae.json".text = builtins.toJSON {
     faviconService = "google";
@@ -37,7 +51,7 @@
     };
     theme = {
       iconTheme = config.gtk.iconTheme.name;
-      name = "custom.json";
+      name = "Custom Theme";
     };
     window = {
       csd = true;
