4a026603797a534030a09b19d4c45b6a2c9f4706
Victor Martinez <knoopx@gmail.com>
2025-10-27 11:19:09 +0100

    feat(niri): meta+enter -> toggle flating to right-bottom corner

--- modules/home-manager/wm/niri/config.nix
+++ modules/home-manager/wm/niri/config.nix
@@ -17,6 +17,32 @@
   # https://github.com/YaLTeR/niri/blob/main/resources/default-config.kdl
 
   xdg.configFile."niri/config.kdl".text = let
+    toggle-float-script = pkgs.writeShellScript "toggle-float" ''
+      window_width=800
+      window_height=600
+      margin=15
+
+      is_floating=$(niri msg --json windows | jq -r '.[] | select(.is_focused == true) | .is_floating')
+
+      if [ "$is_floating" = "true" ]; then
+        niri msg action toggle-window-floating
+        niri msg action set-column-width ${toString (nixosConfig.defaults.display.defaultColumnWidthPercent * 100)}%
+        niri msg action reset-window-height
+      else
+        focused_output=$(niri msg --json workspaces | jq -r '.[] | select(.is_focused == true) | .output')
+        scale=$(niri msg --json outputs | jq -r ".[\"$focused_output\"].logical.scale")
+        width=$(niri msg --json outputs | jq -r ".[\"$focused_output\"].logical.width")
+        height=$(niri msg --json outputs | jq -r ".[\"$focused_output\"].logical.height")
+
+        x=$((width - window_width - margin))
+        y=$((height - window_height - margin))
+
+        niri msg action toggle-window-floating
+        niri msg action set-window-width $window_width
+        niri msg action set-window-height $window_height
+        niri msg action move-floating-window --x $x --y $y
+      fi
+    '';
   in ''
     input {
         keyboard {
@@ -159,7 +185,8 @@
         Mod+O { consume-or-expel-window-right; }
         Mod+Q { close-window; }
         Mod+R { switch-preset-column-width; }
-        Mod+Return { toggle-window-floating; }
+        Mod+Tab { switch-focus-between-floating-and-tiling; }
+        Mod+Return { spawn "${toggle-float-script}"; }
         Mod+Right { focus-column-right; }
         Mod+Shift+Ctrl+L { quit skip-confirmation=true; }
         Mod+Shift+Down { move-column-to-workspace-down; }
@@ -195,16 +222,40 @@
     }
 
     window-rule {
-        match
         draw-border-with-background false
         geometry-corner-radius 8.0 8.0 8.0 8.0
         clip-to-geometry true
+        default-floating-position x=12 y=12 relative-to="bottom-right"
+    }
+
+
+    window-rule {
+        match is-floating=true
+        geometry-corner-radius 16.0 16.0 16.0 16.0
+    }
+
+    window-rule {
+        match app-id=r#"firefox$"# title="^Picture-in-Picture$"
+        open-floating true
+        open-focused false
+        default-floating-position x=32 y=32 relative-to="bottom-right"
+        default-column-width { proportion 0.3; }
+        default-window-height { proportion 0.3; }
+    }
+
+    window-rule {
+        match app-id="scrcpy"
+        open-floating false;
+        default-column-width { fixed 472; }
+        geometry-corner-radius 18.0 18.0 18.0 18.0
     }
 
     window-rule {
         match is-active=false
         opacity 0.9
     }
+
+
 
     layer-rule {
         match namespace="notifications"
@@ -222,13 +273,6 @@
         slowdown 0.6
         window-open { off; }
         window-close { off; }
-    }
-
-    window-rule {
-        match app-id="scrcpy"
-        open-floating false;
-        default-column-width { fixed 472; }
-        geometry-corner-radius 18.0 18.0 18.0 18.0
     }
 
     ${lib.concatStringsSep "\n" (
