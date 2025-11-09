6dffe537c8670abec8cffa1daae58a5fd19edbe3
Victor Martinez <knoopx@gmail.com>
2025-11-09 23:31:49 +0100

    feat(webapps): ✨ add Home Manager webapps module

--- modules/home-manager/programs/firefox/default.nix
+++ modules/home-manager/programs/firefox/default.nix
@@ -8,6 +8,7 @@ in {
     enable = true;
     policies = import ./_policies.nix args;
     profiles."${nixosConfig.defaults.username}" = import ./_profile.nix args;
+    profiles."webapp" = import ./_webapp.nix args;
   };
 
   stylix.targets.firefox.profileNames = ["${nixosConfig.defaults.username}"];
