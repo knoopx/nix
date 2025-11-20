7b784ad826b44d382e0c103c444663be37ea5a8b
Victor Martinez <knoopx@gmail.com>
2025-11-20 19:37:00 +0100

    refactor(nvidia): ♻️ Simplify nvidia configuration
    Refactored the nvidia configuration to streamline the setup. Removed unnecessary environment variables and consolidated boot parameters for clarity.
    
    - Simplified boot.initrd.kernelModules declaration.
    - Removed blacklistedKernelModules as it was not needed.
    - Ensured nvidia-container-toolkit service configuration is included.
    
    This change improves maintainability and readability of the configuration.

--- hosts/desktop/nvidia.nix
+++ hosts/desktop/nvidia.nix
@@ -1,24 +1,12 @@
-{config, ...}: {
-  programs.gamemode.enable = true;
+{
+  lib,
+  config,
+  ...
+}: {
+  # programs.gamemode.enable = true;
 
-  environment = {
-    variables = {
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      EGL_VENDOR_LIBRARY_FILENAMES = "/run/opengl-driver/share/glvnd/egl_vendor.d/10_nvidia.json";
-      GBM_BACKEND = "nvidia-drm";
-      NVD_BACKEND = "direct";
-      NVIDIA_DRIVER_CAPABILITIES = "all";
-      NVIDIA_VISIBLE_DEVICES = "all";
-    };
-  };
-
-  boot = {
-    initrd.kernelModules = [
-      "nvidia nvidia_modeset nvidia_uvm nvidia_drm"
-    ];
-
-    blacklistedKernelModules = ["nouveau"];
-  };
+  boot.initrd.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
+  boot.kernelParams = ["nvidia-drm.modeset=1" "nvidia_drm.fbdev=1"];
   nixpkgs.config.nvidia.acceptLicense = true;
 
   hardware = {
@@ -46,4 +34,5 @@
   };
 
   hardware.nvidia-container-toolkit.enable = true;
+  systemd.services.nvidia-container-toolkit-cdi-generator.serviceConfig.ExecStartPre = lib.mkForce null;
 }
