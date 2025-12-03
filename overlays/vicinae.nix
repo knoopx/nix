2bcfd8ea544a016e89c798c66c599de860549f2f
Victor Martinez <knoopx@gmail.com>
2025-12-03 19:48:29 +0100

    update(vicinae): ⬆️ Bump version to 0.16.11
    Updated the vicinae package to version 0.16.11. This includes changes to the source hash and dependencies to ensure compatibility with the latest features and fixes.
    
    - Updated version from 0.16.8 to 0.16.11.
    - Updated source hash for GitHub fetch.
    - Updated hashes for npm dependencies.

--- overlays/vicinae.nix
+++ overlays/vicinae.nix
@@ -1,19 +1,19 @@
 final: prev: {
   vicinae = prev.vicinae.overrideAttrs (oldAttrs: rec {
-    version = "0.16.8";
+    version = "0.16.11";
     src = prev.fetchFromGitHub {
       owner = "vicinaehq";
       repo = "vicinae";
       tag = "v${version}";
-      hash = "sha256-TAKv3dmc8DSlVp0LXQeLgrgfLTbQ/saQelenFUp9sP0=";
+      hash = "sha256-gX7bUoIP4PU0wUOW3ciyjYAInX/6VLVcEBKdQIQyzDk=";
     };
     apiDeps = prev.fetchNpmDeps {
       src = "${src}/typescript/api";
-      hash = "sha256-4OgVCnw5th2TcXszVY5G9ENr3/Y/eR2Kd45DbUhQRNk=";
+      hash = "sha256-UsTpMR23UQBRseRo33nbT6z/UCjZByryWfn2AQSgm6U=";
     };
     extensionManagerDeps = prev.fetchNpmDeps {
       src = "${src}/typescript/extension-manager";
-      hash = "sha256-krDFHTG8irgVk4a79LMz148drLgy2oxEoHCKRpur1R4=";
+      hash = "sha256-wl8FDFB6Vl1zD0/s2EbU6l1KX4rwUW6dOZof4ebMMO8=";
     };
   });
 }
