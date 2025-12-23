b8fdf383f2e1421f6f8f1199e87d87fe26e5c08e
Victor Martinez <knoopx@gmail.com>
2025-12-23 10:40:05 +0100

    update(fish): ⬆️ Update fish path for bun
    Changed the fish path for bun from `$HOME/.bun/bin` to `$HOME/.cache/.bun/bin` to ensure proper integration with the caching mechanism. This adjustment helps in managing dependencies more effectively and avoids potential conflicts with other installations.

--- modules/home-manager/programs/fish.nix
+++ modules/home-manager/programs/fish.nix
@@ -28,12 +28,11 @@
         set -gx DEEPSEEK_API_KEY (secret-tool lookup deepseek-api key | head | tr -d "\n")
         set -gx GITHUB_PERSONAL_ACCESS_TOKEN (secret-tool lookup github token | head | tr -d "\n")
 
-        fish_add_path -g "$HOME/.bun/bin"
+        fish_add_path -g "$HOME/.cache/.bun/bin"
         fish_add_path -g "$HOME/.cargo/bin:"
         fish_add_path -g "$HOME/.local/bin/"
         fish_add_path -g "$HOME/bin/"
         fish_add_path -g "$HOME/go/bin"
-        fish_add_path -g "$HOME/.opencode/bin"
 
         set -x LD_LIBRARY_PATH "/run/opengl-driver/lib/:$NIX_LD_LIBRARY_PATH"
         set -x LIBRARY_PATH "$LD_LIBRARY_PATH"
