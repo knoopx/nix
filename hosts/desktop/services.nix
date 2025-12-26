644930c992a2f5ca7f38f604adba3db4b5d3f96e
Victor Martinez <knoopx@gmail.com>
2025-12-26 22:54:24 +0100

    remove(android-backup): ➖ Remove Android backup service
    Deleted the Android backup service and its related configurations. This change cleans up the codebase by removing unused features that are no longer needed.

--- hosts/desktop/services.nix
+++ hosts/desktop/services.nix
@@ -1,18 +1,5 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{config, ...}: {
   services = {
-    androidBackup = {
-      enable = true;
-      # backupDir = "/mnt/storage/Photos/input/Android";
-      backupDir = "/home/${config.defaults.username}/Pictures/Camera";
-      screenshotsDir = "/home/${config.defaults.username}/Pictures/Screenshots";
-      downloadsDir = "/home/${config.defaults.username}/Downloads";
-      serialShort = "31051JEHN09244";
-    };
-
     udev.extraRules = ''
       # Disable ASUS USB Audio
       SUBSYSTEM=="usb", ATTR{idVendor}=="0b05", ATTR{idProduct}=="1b9b", ATTR{authorized}="0"
