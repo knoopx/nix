cf7019b9ede1d14b497252a633520564de8799a3
Victor Martinez <knoopx@gmail.com>
2025-12-26 21:17:52 +0100

    update(vicinae): ⬆️ Bump version to 0.17.3
    Updated the vicinae package version from 0.17.1 to 0.17.3. This change includes the updated source hash for the new version, ensuring that the correct version is fetched from GitHub.
    
    No functional changes were made to the dependencies.

--- overlays/vicinae.nix
+++ overlays/vicinae.nix
@@ -1,19 +1,19 @@
 final: prev: {
   vicinae = prev.vicinae.overrideAttrs (oldAttrs: rec {
-    version = "0.17.1";
+    version = "0.17.3";
     src = prev.fetchFromGitHub {
       owner = "vicinaehq";
       repo = "vicinae";
       tag = "v${version}";
-      hash = "sha256-G9zuw0IuzOxCeAcLE+IXcsdp0vAGMXBBdlfjBISnL90=";
+       hash = "sha256-EzvASqrcGZqWyESuYNKRnH17s5hBJK2woIrS6iD6nOs=";
     };
-    apiDeps = prev.fetchNpmDeps {
-      src = "${src}/typescript/api";
-      hash = "sha256-UsTpMR23UQBRseRo33nbT6z/UCjZByryWfn2AQSgm6U=";
-    };
-    extensionManagerDeps = prev.fetchNpmDeps {
-      src = "${src}/typescript/extension-manager";
-      hash = "sha256-wl8FDFB6Vl1zD0/s2EbU6l1KX4rwUW6dOZof4ebMMO8=";
-    };
+     apiDeps = prev.fetchNpmDeps {
+       src = "${src}/typescript/api";
+       hash = "sha256-UsTpMR23UQBRseRo33nbT6z/UCjZByryWfn2AQSgm6U=";
+     };
+     extensionManagerDeps = prev.fetchNpmDeps {
+       src = "${src}/typescript/extension-manager";
+       hash = "sha256-wl8FDFB6Vl1zD0/s2EbU6l1KX4rwUW6dOZof4ebMMO8=";
+     };
   });
 }
