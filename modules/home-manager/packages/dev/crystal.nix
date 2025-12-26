acc3b929d89ba46c71b8d0a7d3edd446c52d9a7b
Victor Martinez <knoopx@gmail.com>
2025-12-26 21:37:48 +0100

    remove(crystal): ➖ Remove mint package configuration
    Removed the mint package configuration from the crystal.nix file. This change simplifies the package list and eliminates unnecessary complexity related to the mint package's versioning and source fetching.

--- modules/home-manager/packages/dev/crystal.nix
+++ modules/home-manager/packages/dev/crystal.nix
@@ -3,16 +3,5 @@
     crystal
     crystalline
     shards
-    (mint.overrideAttrs
-      rec {
-        version = "0.27.0";
-        doCheck = false;
-        src = fetchFromGitHub {
-          owner = "mint-lang";
-          repo = "mint";
-          tag = version;
-          hash = "sha256-nmQzJ+LkNJ3WSW1F59vRQpNiYgml4xkTj745vWPPUEE=";
-        };
-      })
   ];
 }
