07f3b95c0aef5913d129206a4992c3d1c491195d
Victor Martinez <knoopx@gmail.com>
2026-01-03 16:18:07 +0100

    config(wezterm): 🔧 Update wezterm configuration
    Enhanced the wezterm configuration by adding key bindings for the Backspace key with CTRL modifier. This change improves user experience by allowing easier navigation and command input.
    
    - Added key binding for Backspace with CTRL.

--- modules/home-manager/programs/wezterm.nix
+++ modules/home-manager/programs/wezterm.nix
@@ -5,11 +5,18 @@
       local wezterm = require 'wezterm'
       local config = wezterm.config_builder()
 
-      return {
-        automatically_reload_config = true,
-        window_close_confirmation = "NeverPrompt",
-        hide_tab_bar_if_only_one_tab = true,
-      }
+       return {
+         automatically_reload_config = true,
+         window_close_confirmation = "NeverPrompt",
+         hide_tab_bar_if_only_one_tab = true,
+         keys = {
+           {
+             key = 'Backspace',
+             mods = 'CTRL',
+             action = wezterm.action.SendString('\x17'),
+           },
+         },
+       }
     '';
   };
 }
