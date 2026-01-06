0e4fd4eced839934b5a4d27b8f8c383eb31f6c16
Victor Martinez <knoopx@gmail.com>
2026-01-06 10:35:37 +0100

    update(vicinae): ⬆️ Bump version to 0.18.1
    Updated the vicinae package version from 0.18.0 to 0.18.1. This includes fetching the new source from GitHub with the updated tag and hash.
    
    - Changed version to "0.18.1".
    - Updated source hash for the new version.

--- overlays/vicinae.nix
+++ overlays/vicinae.nix
@@ -1,12 +1,12 @@
 final: prev: {
-  vicinae = prev.vicinae.overrideAttrs (oldAttrs: rec {
-    version = "0.18.0";
-    src = prev.fetchFromGitHub {
-      owner = "vicinaehq";
-      repo = "vicinae";
-      tag = "v${version}";
-      hash = "sha256-ApMcDKe+6uYb2M+UL8SWW8M1S5bmT8EI5uOChLxzWqs=";
-    };
+   vicinae = prev.vicinae.overrideAttrs (oldAttrs: rec {
+     version = "0.18.1";
+     src = prev.fetchFromGitHub {
+       owner = "vicinaehq";
+       repo = "vicinae";
+       tag = "v${version}";
+       hash = "sha256-I7FKl8r8xtDfQVo4kkYQEKsxpFhSRZUak67LEHmSyY8=";
+     };
       apiDeps = prev.fetchNpmDeps {
         src = "${src}/typescript/api";
         hash = "sha256-UsTpMR23UQBRseRo33nbT6z/UCjZByryWfn2AQSgm6U=";
