162cd841915a3c3204cda2a69333e7f9c9cf1623
Victor Martinez <knoopx@gmail.com>
2026-01-10 19:38:53 +0100

    add(zed-editor): ➕ Add initial configuration for Zed Editor
    Introduced a new configuration file for Zed Editor, setting default user settings, themes, and features. This includes custom themes and an edit prediction provider.
    
    - Disabled the editor by default.
    - Configured user settings for themes and fonts.
    - Set up features for edit prediction using Copilot.

---
+++ modules/home-manager/programs/zed-editor.nix
@@ -0,0 +1,35 @@
+{
+  nixosConfig,
+  lib,
+  ...
+}: {
+  programs.zed-editor = {
+    enable = false;
+    extensions = [];
+    extraPackages = [];
+    userSettings = {
+      theme = "Base16 custom";
+      icon_theme = "Catppuccin Mocha";
+      features = {
+        edit_prediction_provider = "copilot";
+      };
+      agent = {
+        always_allow_tool_actions = true;
+        default_model = {
+          provider = "copilot_chat";
+        };
+      };
+      telemetry = {
+        metrics = false;
+      };
+      ui_font_size = lib.mkForce 12;
+      buffer_font_size = lib.mkForce 11;
+      buffer_font_family = nixosConfig.defaults.fonts.monospace.name;
+      ui_font_family = nixosConfig.defaults.fonts.sansSerif.name;
+      vim_mode = false;
+    };
+    userKeymaps = [];
+    userTasks = [];
+    installRemoteServer = false;
+  };
+}
