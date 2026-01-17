7bd9e8d9c0c6652b17e3dfb15e1acb2b2d22fbe0
Victor Martinez <knoopx@gmail.com>
2026-01-18 00:48:21 +0100

    config(nu-shell): add plugins

--- modules/home-manager/packages/dev/nu-shell.nix
+++ modules/home-manager/packages/dev/nu-shell.nix
@@ -1,6 +1,11 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     nushell
+    nushellPlugins.polars
+    nushellPlugins.query
+    nushellPlugins.desktop_notifications
+    nushellPlugins.highlight
+    nushellPlugins.skim
     nufmt
   ];
 }
