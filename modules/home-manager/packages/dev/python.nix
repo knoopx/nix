8dede39647eed26eda19e7102139f88d2b480e81
Victor Martinez <knoopx@gmail.com>
2026-01-18 09:51:08 +0100

    config(python): add datawrangler deps

--- modules/home-manager/packages/dev/python.nix
+++ modules/home-manager/packages/dev/python.nix
@@ -4,6 +4,18 @@
     black
     isort
     pyright
-    python313
+    (python313.withPackages (ps: [
+      ps.pygobject3
+      ps.pillow
+      ps.pygobject-stubs
+      ps.mypy
+      ps.pytest
+      ps.pytest-cov
+      ps.ipykernel
+      ps.ipython
+      ps.ipywidgets
+      ps.pandas
+      ps.pyarrow
+    ]))
   ];
 }
