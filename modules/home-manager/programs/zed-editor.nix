41e6184749cd5a7bb7e1983da864000075c7cdf9
Victor Martinez <knoopx@gmail.com>
2026-01-20 22:45:57 +0100

    config(zed): update settings

--- modules/home-manager/programs/zed-editor.nix
+++ modules/home-manager/programs/zed-editor.nix
@@ -8,24 +8,41 @@
     extensions = [];
     extraPackages = [];
     userSettings = {
-      theme = "Base16 custom";
-      icon_theme = "Catppuccin Mocha";
-      features = {
-        edit_prediction_provider = "copilot";
+      ui_font_size = lib.mkForce 16;
+      buffer_font_size = lib.mkForce 12;
+      buffer_font_family = "JetBrainsMono Nerd Font";
+      ui_font_weight = 400;
+      buffer_font_weight = 300;
+      buffer_line_height = "standard";
+      terminal = {
+        font_size = 11;
+        font_weight = 100;
+        line_height = "standard";
       };
+      agent_servers = {
+        OpenCode = {
+          type = "custom";
+          command = "/etc/profiles/per-user/${nixosConfig.defaults.username}/bin/oc";
+          args = ["acp"];
+          env = {};
+        };
+      };
+      context_servers = {};
       agent = {
         always_allow_tool_actions = true;
         default_model = {
+          model = "gpt-4.1";
           provider = "copilot_chat";
         };
       };
+      features = {
+        edit_prediction_provider = "copilot";
+      };
+      icon_theme = "Catppuccin Mocha";
       telemetry = {
         metrics = false;
       };
-      ui_font_size = lib.mkForce 12;
-      buffer_font_size = lib.mkForce 11;
-      buffer_font_family = nixosConfig.defaults.fonts.monospace.name;
-      ui_font_family = nixosConfig.defaults.fonts.sansSerif.name;
+      theme = "Base16 custom";
       vim_mode = false;
     };
     userKeymaps = [];
