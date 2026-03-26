b18236e6af17196ba643d7f92e07c45ed64d95f1
Victor Martinez <knoopx@gmail.com>
2026-03-26 13:41:26 +0100

    feat(container): ⚙️ update vLLM container configuration

--- hosts/desktop/containers/qwen-vllm.nix
+++ hosts/desktop/containers/qwen-vllm.nix
@@ -1,6 +1,7 @@
 {config, ...}: let
   name = "qwen-vllm";
   model = "apolo13x/Qwen3.5-27B-NVFP4";
+  # model = "mconcat/Qwen3.5-27B-Claude-4.6-Opus-Reasoning-Distilled-NVFP4";
   modelCache = "/home/${config.defaults.username}/.cache/huggingface";
 in {
   virtualisation.oci-containers.containers."${name}" = {
@@ -14,13 +15,28 @@ in {
       "Qwen3.5-27B-NVFP4"
 
       "--max-model-len"
-      # "234567"
       "131072"
+      # "234567"
       # "--max-model-len" "262144"
+      "--max-num-seqs"
+      "1" # 4
+      # "--max-num-batched-tokens" "4096"
+      "--tensor-parallel-size"
+      "1"
+      # "--enable-chunked-prefill"
+      # "--enable-prefix-caching"
+      # "--load-format"
+      # "fastsafetensors"
+
+      "--attention-backend"
+      "flashinfer"
+      "--language-model-only"
+      "--skip-mm-profiling"
+
       "--kv-cache-dtype"
       "fp8_e4m3"
       "--gpu-memory-utilization"
-      "0.9"
+      "0.92"
 
       "--reasoning-parser"
       "qwen3"
@@ -28,24 +44,11 @@ in {
       "--tool-call-parser"
       "qwen3_coder"
 
-      "--language-model-only"
-      # "--enable-prefix-caching"
-
-      "--skip-mm-profiling"
-      "--max-num-seqs"
-      "1"
-
-      # "--enable-chunked-prefill"
-
-      # "--tensor-parallel-size"
-      # "1"
       # "--trust-remote-code"
-      # "--max-num-seqs=4"
-      # "--max-num-batched-tokens=4096"
 
       # "--enforce-eager"
-      # "--speculative-config"
-      # ''{"method":"mtp","num_speculative_tokens":1}''
+      "--speculative-config"
+      ''{"method":"mtp","num_speculative_tokens":2}''
     ];
 
     ports = ["11434:8000"];
@@ -55,9 +58,14 @@ in {
       HF_HOME = "/root/.cache/huggingface";
       CUDA_DEVICE_ORDER = "PCI_BUS_ID";
       # VLLM_NVFP4_GEMM_BACKEND = "marlin";
-      # VLLM_NVFP4_GEMM_BACKEND = "flashinfer-cutlass"; #
+      # VLLM_NVFP4_GEMM_BACKEND = "flashinfer-cutlass";
       VLLM_NVFP4_GEMM_BACKEND = "cutlass";
       VLLM_USE_FLASHINFER_MOE_FP4 = "1";
+
+      # VLLM_WORKER_MULTIPROC_METHOD = "spawn";
+      # SAFETENSORS_FAST_GPU = "1";
+      # VLLM_DISABLE_PYNCCL = "1";
+      # NCCL_IB_DISABLE = "1";
     };
 
     extraOptions = ["--device=nvidia.com/gpu=all" "--ipc=host"];
