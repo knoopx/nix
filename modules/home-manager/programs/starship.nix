036bba2a9d0408dfc3414841f839dda143faaf98
Victor Martinez <knoopx@gmail.com>
2026-03-27 18:51:56 +0100

    feat(hm/starship): ⭐ add jujutsu status and diff stats to prompt

--- modules/home-manager/programs/starship.nix
+++ modules/home-manager/programs/starship.nix
@@ -8,6 +8,21 @@
     settings = {
       add_newline = true;
       format = "$all";
+
+      git_branch.disabled = true;
+      git_commit.disabled = true;
+      git_metrics.disabled = true;
+      git_state.disabled = true;
+      git_status.disabled = true;
+
+      custom.jj = {
+        format = "on [$symbol $output]($style) ";
+        command = ''
+          jj log -r'ancestors(@) & bookmarks()' --no-graph -n1 -T 'bookmarks.join(", ")'
+        '';
+        when = "jj --ignore-working-copy root";
+        symbol = "󰃀";
+      };
     };
   };
 }
