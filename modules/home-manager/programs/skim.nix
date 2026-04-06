53fd07bf8d5e9cf6f1f148341f35334833593e96
Victor Martinez <knoopx@gmail.com>
2026-04-06 15:48:30 +0200

    style(skim): refactor color configuration using let binding

--- modules/home-manager/programs/skim.nix
+++ modules/home-manager/programs/skim.nix
@@ -1,8 +1,10 @@
-{nixosConfig, ...}: {
+{nixosConfig, ...}: let
+  colors = nixosConfig.defaults.colorScheme.palette;
+in {
   programs.skim = {
     enable = true;
-    defaultOptions = with nixosConfig.defaults.colorScheme.palette; [
-      "--color=fg:#${base05},bg:#${base00},matched:#${base02},matched_bg:#${base0F},current:#${base05},current_bg:#${base03},current_match:#${base00},current_match_bg:#${base06},spinner:#${base0B},info:#${base0E},prompt:#${base0D},cursor:#${base08},selected:#${base0F},header:#${base0C},border:#${base04}"
+    defaultOptions = [
+      "--color=fg:#${colors.base05},bg:#${colors.base00},matched:#${colors.base0D},matched_bg:#${colors.base01},current:#${colors.base05},current_bg:#${colors.base02},current_match:#${colors.base05},current_match_bg:#${colors.base0D},spinner:#${colors.base05},info:#${colors.base04},prompt:#${colors.base0D},cursor:#${colors.base0D},selected:#${colors.base0D},header:#${colors.base0D},border:#${colors.base02}"
     ];
   };
 }
