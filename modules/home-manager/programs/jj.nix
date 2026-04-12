52a1e9fd5edfa923ff6877467e62d09627ac68a2
Victor Martinez <knoopx@gmail.com>
2026-04-12 21:01:36 +0200

    refactor(jj): restructure merge-tools configuration with weave as nested key

--- modules/home-manager/programs/jj.nix
+++ modules/home-manager/programs/jj.nix
@@ -9,12 +9,14 @@
       editor = nixosConfig.defaults.editor;
       merge-editor = "weave";
     };
-    "merge-tools.weave" = {
-      program = "weave-driver";
-      merge-args = ["$base" "$left" "$right" "-o" "$output" "-l" "$marker_length" "-p" "$path"];
-      merge-conflict-exit-codes = [1];
-      merge-tool-edits-conflict-markers = true;
-      conflict-marker-style = "git";
+    "merge-tools" = {
+      weave = {
+        program = "weave-driver";
+        merge-args = ["$base" "$left" "$right" "-o" "$output" "-l" "$marker_length" "-p" "$path"];
+        merge-conflict-exit-codes = [1];
+        merge-tool-edits-conflict-markers = true;
+        conflict-marker-style = "git";
+      };
     };
   };
 }
