89933737d0bbec1fd65b34d24546780db84bbb7e
Victor Martinez <knoopx@gmail.com>
2026-04-13 02:09:23 +0200

    chore(glance): cleanup YouTube channels list and remove RSS feeds

--- hosts/desktop/home-manager/glance/pages.nix
+++ hosts/desktop/home-manager/glance/pages.nix
@@ -1,8 +1,8 @@
-{
-  config,
-  nixosConfig,
-  ...
-}: let
+{ config
+, nixosConfig
+, ...
+}:
+let
   homePage = {
     name = "Home";
     columns = [
@@ -13,7 +13,7 @@
             type = "weather";
             location = nixosConfig.defaults.location;
           }
-          {type = "calendar";}
+          { type = "calendar"; }
           {
             type = "custom-api";
             title = "Vilassar de Mar Departures";
@@ -104,10 +104,10 @@
             type = "rss";
             cache = "5m";
             feeds = [
-              {url = "https://community.silverbullet.md/latest.rss";}
-              {url = "https://discourse.nixos.org/latest.rss";}
-              {url = "https://towardsdatascience.com/feed";}
-              {url = "https://huggingface.co/blog/feed.xml";}
+              { url = "https://community.silverbullet.md/latest.rss"; }
+              { url = "https://discourse.nixos.org/latest.rss"; }
+              { url = "https://towardsdatascience.com/feed"; }
+              { url = "https://huggingface.co/blog/feed.xml"; }
             ];
           }
 
@@ -220,81 +220,17 @@
             type = "videos";
             cache = "15m";
             channels = [
-              "UC9TM3Lrth8MQjHrttZJZiEw" # Adam Liaw
-              "UCCWeRTgd79JL0ilH0ZywSJA" # AlphaPhoenix
-              "UC8zTlrhQ0w1-TZjc2-jdcag" # Andre Antunes
-              "UCV0t1y4h_6-2SqEpXBXgwFQ" # AngeTheGreat
-              "UCDF3Sd2LNAsa-nKD17Jq3mw" # Bambu Lab
-              "UC4NNPgQ9sOkBjw6GlkgCylg" # Ben Vallack
-              "UC2Tc0TsvFxC83zF1w5x1PWQ" # Breaks'n'Makes
-              "UCRs9vVRoVo4BTBceFRaJnpA" # Brian Brocken
-              "UC0A0TIq-WDyKZcGcOt5WpPA" # Brothers Make
-              "UC45_6KVAAZG_iOgzksGswMw" # Chris Rosser
-              "UCUQo7nzH1sXVpzL92VesANw" # DIY Perks
-              "UCNvZxz-G-MDx6hdrT0AcmfA" # DeltaGeek
-              "UCCeQLxZHazu4RFqlMmLAw-A" # Donn DIY
-              "UCYmna5rFHIesFteksAvFOfg" # Dr. Plants
-              "UCSGjvsXMtI70uSvabadwRJg" # Duke Doks
-              "UCI2HDanVxEp54l5AQqZor3g" # E3D
-              "UCPDis9pjXuqyI7RYLJ-TTSA" # FailArmy
               "UCVveEFTOd6khhSXXnRhxJmg" # Fireball Tool
-              "UCWKqh_3cb6cDiz2ICWc-5Sw" # Gear Show
               "UC6mIxFTvXkWQVEHPsEdflzQ" # GreatScott!
-              "UC-nPM1_kSZf91ZGkcgy_95Q" # How to ADHD
               "UC2avWDLN1EI3r1RZ_dlSxCw" # Integza
               "UCSksodwjNeOQ3Oywr1G4Nag" # JBV Creative
               "UCHk0IYEDJxQGQ-nbd3yP_KA" # Joel Creates
-              "UCJK1WpPzUjz2uIRahllinvw" # Joerg Beigang
-              "UC7O8KgJdsE_e9op3vG-p2dg" # Johnny FPV
-              "UCX-fEgMLE56OYqEdOpyob7w" # Keith57000
-              "UC5rT7F0PGNuD54rJ9kzgWzw" # Kris Harbour Natural Building
-              "UCEZME1N4Be_m_vp_pc3zlAA" # KyleRoyerKnives
-              "UCJ__W0hroWZTdBA9n96Z8Sg" # MORE Maker's Muse
-              "UCRVQnEnQzkhXZ4YiOvKk_TQ" # MW Restoration
-              "UCWYV5hJfUrnU9Ishf5vr2KA" # Make It RC
-              "UCYfrql-Noh09uq7SURsevTA" # Makerfire
               "UC1O0jDlG51N3jGf6_9t-9mw" # Marco Reps
-              "UCRP4g0sUktEbodGx6EUr7mQ" # Matteo Villani
-              "UCAVqrVBd18fBnyiyt_wPkoQ" # Mediocre Maker
-              "UCBGpbEe0G9EchyGYCRRd4hg" # Nick Burns
-              "UCQ3OvT0ZSWxoVDjZkVNmnlw" # Oscar Liang
-              "UC2seKgMkTx-ZR1vTT6vUvDw" # Paul's Projects
-              "UCUcWguV9it93wMVzkOBPrAQ" # Penguin DIY
               "UC7yF9tV4xWEMZkel7q8La_w" # PeterSripol
-              "UC08sUqoDgLKhR4sbNHv3wKA" # Pilotgeek
-              "UCPCw5ycqW0fme1BdvNqOxbw" # ProjectAir
-              "UCLHAxAdvAKJY0niRJZRYMvg" # Prusa 3D by Josef Prusa
               "UCKd49wwdEdIoM-7Fumhwmog" # Quint BUILDs
-              "UCZ22g8sMSoQIF8CnFcNQVXA" # RC Rock Crawler
-              "UCX-Fe4LiUYaC6ZzlHvSCfow" # RC Scientific
-              "UC873OURVczg_utAk8dXx_Uw" # RCLifeOn
-              "UCQVGC2PuLRlS_uKmcNUPReg" # Rat Rig
-              "UC4a9LfdavRlVMaSSWFdIciA" # RobWords
-              "UCUmG1cB1y35-IpvYZj4t49Q" # SR13_fpv
-              "UCMV6M_1IE8y8wr6kEF2Dsuw" # Sciencish
-              "UCaFeo-xnmdThDRihD1v39Vg" # Scott Tomlinson
-              "UCE-bw6PRKuDlH6fP1mP4nOw" # SeanHodgins
-              "UCDJo4LLCjlbupu1DMgvNlDw" # Sent and Bent
-              "UC3KEoMzNz8eYnwBC34RaKCQ" # Simone Giertz
-              "UC0ng5CCqYTvN0WTwwXYU4Qw" # Smoking Room
               "UCEIwxahdLz7bap-VDs9h35A" # Steve Mould
-              "UCNolGmzBR60IZfS9z7LlNEw" # Teenenggr
-              "UCFRE4F1oMhKXb4RylmF3Bxw" # Texas BUSHMAN
               "UC5UAwBUum7CPN5buc-_N1Fw" # The Linux Experiment
-              "UCZdGJgHbmqQcVZaJCkqDRwg" # The Q
-              "UCMZkAT1Vf7CPSNcTzZkK_qQ" # Think Flight
-              "UCQSpnDG3YsFNf5-qHocF-WQ" # ThioJoe
-              "UC5NO8MgTQKHAWXp6z8Xl7yQ" # This Old Tony
-              "UCEaxX1yQlFPmAKbE6AFpBWQ" # Thánh Chế - Mr Hồ
-              "UCZYvU-ZWbgl1rJv0trRk_fA" # Tim Station
               "UC67gfx2Fg7K2NSHqoENVgwA" # Tom Stanton
-              "UC4Q7GgIQ4kFXnZ3XIBFgUig" # Traxxas
-              "UCI2MZOaHJFMAmW5ni7vuAQg" # UAV Tech
-              "UCUW49KGPezggFi0PGyDvcvg" # Zack Freedman
-              "UCZWadyLVO4ZnMgLrRVtS6VA" # baby WOGUE
-              "UCMrMVIBtqFW6O0-MWq26gqw" # my mechanics
-              "UCNC9gjgYrzLfyMQmp31DnjA" # myfordboy
-              "UC7c1iZQpR8_mct1tCKcjWcw" # quadmovr
               "UC1D3yD4wlPMico0dss264XA" # NileBlue
               "UC1VLQPn9cYSqx8plbk9RxxQ" # The Action Lab
               "UCdC0An4ZPNr_YiFiYoVbwaw" # Daily Dose Of Internet
@@ -302,7 +238,6 @@
               "UCj1VqrHhDte54oLgPG4xpuQ" # Stuff Made Here
               "UCq2rNse2XX4Rjzmldv9GqrQ" # rctestflight
               "UCsBjURrPoezykLs9EqgamOA" # Fireship
-              "UCu-NRUdNtfcjdGA5Abt3JUw" # Mr. Sujano
             ];
           }
 
@@ -470,7 +405,8 @@
       }
     ];
   };
-in {
+in
+{
   # https://github.com/glanceapp/glance/blob/main/docs/extensions.md
   config = {
     services.glance.settings.pages = [
