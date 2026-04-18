3d5dfd7a376220b76512031b0b51ad3a59f5bae4
Victor Martinez <knoopx@gmail.com>
2026-04-18 23:56:01 +0200

    feat(nu-shell): add helix-aligned keybindings

--- modules/home-manager/programs/nu-shell/default.nix
+++ modules/home-manager/programs/nu-shell/default.nix
@@ -13,6 +13,175 @@ in {
     enable = true;
     configFile = {
       text = ''
+        # Keybindings (aligned with helix/kitty conventions)
+        $env.config.keybindings --+= [
+          # Clear screen / Quit
+          {
+            name: "clear_screen"
+            modifier: control
+            keycode: char_l
+            mode: emacs
+            event: { send: clearscreen }
+          }
+          {
+            name: "quit_shell"
+            modifier: control
+            keycode: char_q
+            mode: emacs
+            event:
+              { send: executehostcommand
+                cmd: "exit"
+              }
+          }
+
+          # Undo/Redo
+          {
+            name: "undo"
+            modifier: control
+            keycode: char_z
+            mode: emacs
+            event: { edit: undo }
+          }
+          {
+            name: "redo"
+            modifier: shift_control
+            keycode: char_z
+            mode: emacs
+            event: { edit: redo }
+          }
+
+          # Search (like helix Ctrl-f)
+          {
+            name: "search_history"
+            modifier: control
+            keycode: char_f
+            mode: emacs
+            event: { send: searchhistory }
+          }
+
+          # Clipboard (system clipboard)
+          {
+            name: "copy_to_clipboard"
+            modifier: control
+            keycode: char_c
+            mode: emacs
+            event: { edit: copselectionsystem }
+          }
+          {
+            name: "paste_from_clipboard"
+            modifier: control
+            keycode: char_v
+            mode: emacs
+            event: { edit: pastesystem }
+          }
+
+          # Text selection
+          {
+            name: "select_all"
+            modifier: control
+            keycode: char_a
+            mode: emacs
+            event: { edit: selectall }
+          }
+
+          # Word navigation (like helix)
+          {
+            name: "move_word_left"
+            modifier: control
+            keycode: left
+            mode: emacs
+            event: { edit: movewordleft }
+          }
+          {
+            name: "move_word_right"
+            modifier: control
+            keycode: right
+            mode: emacs
+            event: { edit: movewordright }
+          }
+
+          # Line navigation (like helix)
+          {
+            name: "goto_line_start"
+            modifier: none
+            keycode: home
+            mode: emacs
+            event: { edit: movetolinestart }
+          }
+          {
+            name: "goto_line_end"
+            modifier: none
+            keycode: end
+            mode: emacs
+            event: { edit: movetolineend }
+          }
+
+          # File start/end (like helix)
+          {
+            name: "goto_file_start"
+            modifier: control
+            keycode: home
+            mode: emacs
+            event: { edit: movetostart }
+          }
+          {
+            name: "goto_last_line"
+            modifier: control
+            keycode: end
+            mode: emacs
+            event: { edit: movetoend }
+          }
+
+          # Page navigation (like helix)
+          # Note: reedline has no page-up/page-down events; these scroll menu items only
+          {
+            name: "page_up"
+            modifier: none
+            keycode: pageup
+            mode: emacs
+            event: { send: menuprevious }
+          }
+          {
+            name: "page_down"
+            modifier: none
+            keycode: pagedown
+            mode: emacs
+            event: { send: menunext }
+          }
+
+          # Delete (like helix)
+          {
+            name: "delete_word_backward"
+            modifier: control
+            keycode: backspace
+            mode: emacs
+            event: { edit: backspaceword }
+          }
+          {
+            name: "delete_word_forward"
+            modifier: control
+            keycode: delete
+            mode: emacs
+            event: { edit: deleteword }
+          }
+
+          # Indent/Outdent (like helix)
+          {
+            name: "indent"
+            modifier: none
+            keycode: tab
+            mode: emacs
+            event: { edit: indent }
+          }
+          {
+            name: "outdent"
+            modifier: shift
+            keycode: tab
+            mode: emacs
+            event: { edit: outdent }
+          }
+        ]
+
         let fish_completer = {|spans|
             fish --command $"complete '--do-complete=($spans | str replace --all "'" "\\'" | str join ' ')'"
             | from tsv --flexible --noheaders --no-infer
