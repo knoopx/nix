8c69ae6763c73bf1fad5751e63afb4a19a48149d
Victor Martinez <knoopx@gmail.com>
2026-04-28 20:17:19 +0200

    fix(theming): update MoreWaita icon builder for new Adwaita-colors directory structure

--- builders/theming/mkMoreWaitaIconTheme.nix
+++ builders/theming/mkMoreWaitaIconTheme.nix
@@ -2,9 +2,23 @@
   pkgs,
   adwaita-colors,
   ...
-}: colorScheme:
+}: _colorScheme:
 pkgs.morewaita-icon-theme.overrideAttrs (prev: {
-  postInstall = ''
-    cp -r ${adwaita-colors}/Adwaita-blue/* $out/share/icons/MoreWaita
+  postInstall = let
+    color = "blue";
+  in ''
+    # Copy colored app icons
+    mkdir -p $out/share/icons/MoreWaita/scalable/apps
+    cp ${adwaita-colors}/apps/${color}/*.svg $out/share/icons/MoreWaita/scalable/apps/
+
+    # Copy colored mimetype icons
+    mkdir -p $out/share/icons/MoreWaita/scalable/mimetypes
+    cp ${adwaita-colors}/mimetypes/${color}/*.svg $out/share/icons/MoreWaita/scalable/mimetypes/
+
+    # Copy custom folder icons
+    mkdir -p $out/share/icons/MoreWaita/scalable/places
+    cp ${adwaita-colors}/folders/*.svg $out/share/icons/MoreWaita/scalable/places/
+
+    gtk-update-icon-cache -f $out/share/icons/MoreWaita
   '';
 })
