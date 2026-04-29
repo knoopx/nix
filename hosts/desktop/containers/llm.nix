ac6ebd738eb405862d520eb7eabcd07f13498c47
Victor Martinez <knoopx@gmail.com>
2026-04-29 23:42:33 +0200

    chore(llm): tune llama.cpp presets and switch Qwen3.6-27B to UD-Q4_K_XL GGUF

--- hosts/desktop/containers/llm.nix
+++ hosts/desktop/containers/llm.nix
@@ -2,25 +2,26 @@
 let
   presets = pkgs.writeText "presets.ini" ''
     [*]
-    flash-attn = on
-    no-mmap = true
+    # flash-attn = on
+    # no-mmap = true
+    ngl = 999
     no-warmup = true
     jinja = on
     parallel = 1
 
-    threads = 64
-    prio = 2
+    # threads = 64
+    # prio = 2
 
     cache-type-k = q4_0
     cache-type-v = q4_0
 
-    batch-size = 4096
-    ubatch-size = 2048
+    # batch-size = 4096
+    # ubatch-size = 2048
 
     [Qwen/Qwen3.6-27B]
     alias = Qwen3.6-27B
-    # hf-repo = unsloth/Qwen3.6-27B-GGUF:UD-Q4_K_XL
-    hf-repo = Freenixi/Abiray-Qwen3.6-27B-NVFP4-GGUF:NVFP4
+    hf-repo = unsloth/Qwen3.6-27B-GGUF:UD-Q4_K_XL
+    # hf-repo = Freenixi/Abiray-Qwen3.6-27B-NVFP4-GGUF:NVFP4
     # hf-repo = DavidAU/Qwen3.6-27B-NEO-CODE-Di-IMatrix-MAX-GGUF:Q5_K_M
     ctx-size = 262144
     temp = 0.8
@@ -34,6 +35,7 @@ let
     alias = Qwen3.6-35B-A3B
     # hf-repo = AesSedai/Qwen3.6-35B-A3B-GGUF:Q5_K_M
     hf-repo = unsloth/Qwen3.6-35B-A3B-GGUF:UD-Q4_K_XL
+    # hf-repo = knoopx/Qwen3.6-35B-A3B-NVFP4-GGUF
     ctx-size = 262144
     temp = 0.8
     top-p = 0.95
@@ -41,7 +43,7 @@ let
     min-p = 0.0
     presence-penalty = 1.5
     repeat-penalty = 1.0
-    override-tensor = blk\.(3[5-9])\.ffn_.*_exps.*=CPU
+    # override-tensor = blk\.(3[5-9])\.ffn_.*_exps.*=CPU
   '';
 in
 {
