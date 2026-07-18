a7b052acf0302c2ade0f20cbb494c9c8a5d1768d
Victor Martinez <knoopx@gmail.com>
2026-07-18 23:54:56 +0200

    refactor(desktop): move plex service config from shared modules and add traefik reverse proxy

--- hosts/desktop/services/plex.nix
+++ hosts/desktop/services/plex.nix
@@ -1,7 +1,13 @@
-{lib, ...}: {
+{ lib, ... }: {
   systemd.services.plex.serviceConfig = {
     KillSignal = lib.mkForce "SIGKILL";
     TimeoutStopSec = 10;
     ProtectHome = lib.mkForce false;
+  };
+
+  services.traefik-proxy = {
+    hostServices = {
+      plex = 32400;
+    };
   };
 }
