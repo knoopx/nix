3f50ae7fe6aa3cf100aebe31baef8f680e0572b0
Victor Martinez <knoopx@gmail.com>
2026-07-19 01:21:22 +0200

    feat(minibookx): add openssh service with root login enabled and password authentication disabled

--- hosts/minibookx/services.nix
+++ hosts/minibookx/services.nix
@@ -1,11 +1,19 @@
-{...}: {
+{ lib, ... }: {
   services.power-profiles-daemon.enable = true;
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = lib.mkForce "yes";
+      PasswordAuthentication = false;
+    };
+  };
 
   services.keyd = {
     enable = true;
     keyboards = {
       minibook = {
-        ids = ["0001:0001"];
+        ids = [ "0001:0001" ];
         settings = {
           main = {
             leftalt = "overload(meta, M-.)";
