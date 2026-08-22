dfe08069f90afb9ab4107bceeefe41d2f8405715
Victor Martinez <knoopx@gmail.com>
2026-08-22 18:10:45 +0200

    chore(deps): bump sem from 0.21.0 to 0.23.0

--- pkgs/sem.nix
+++ pkgs/sem.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}:
 pkgs.stdenv.mkDerivation rec {
   pname = "sem";
-  version = "0.21.0";
+  version = "0.23.0";
 
   src = pkgs.fetchurl {
     url = "https://github.com/Ataraxy-Labs/sem/releases/download/v${version}/sem-linux-x86_64.tar.gz";
-    hash = "sha256-SgbwGVUq3Te0sGkzCdqvUp6ufykSF9IMKRKUx5Cxa0s=";
+    hash = "sha256-W9vS1LR2e+7Egl9fWDvNvXFaJeNONgLyl6dPrVL9qu4=";
   };
 
   sourceRoot = ".";
