0d4f6a0c59e29356c29d5c61ade322c7bdc2465e
Victor Martinez <knoopx@gmail.com>
2026-09-04 18:25:47 +0200

    chore(deps): bump weave from 0.5.2 to 0.5.4

--- pkgs/weave.nix
+++ pkgs/weave.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}: let
   pname = "weave";
-  version = "0.5.2";
+  version = "0.5.4";
 
   src = pkgs.fetchFromGitHub {
     owner = "Ataraxy-Labs";
     repo = "weave";
     rev = "v${version}";
-    hash = "sha256-Aj2fWvur1N2VnVlPJHqg0L5hiWGdthznkTvIzWgGlIg=";
+    hash = "sha256-en8HwzvC2uPBwyHnQyUHrRLvWyWDWPptfTpX353i/pU=";
   };
 in
   pkgs.rustPlatform.buildRustPackage {
@@ -14,7 +14,7 @@ in
 
     subPackages = ["crates/weave-cli" "crates/weave-driver"];
 
-    cargoHash = "sha256-erodWNZ/E50ND2F1UXSYd5LXi8A4EIaUNoJYld6KJjQ=";
+    cargoHash = "sha256-LYcHCc3OkBmWY9tSpm3Mp+Dw/CRoTICngL7+GkUDAHk=";
 
     nativeBuildInputs = with pkgs; [
       pkg-config
