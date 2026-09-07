05a408aee2427f6f4939ef3392b3c7a4d36818a1
Victor Martinez <knoopx@gmail.com>
2026-09-07 07:50:26 +0200

    chore(deps): bump wacli to 0.17.2

--- pkgs/wacli.nix
+++ pkgs/wacli.nix
@@ -1,18 +1,18 @@
 {pkgs}: let
   pname = "wacli";
-  version = "0.17.1";
+  version = "0.17.2";
 
   src = pkgs.fetchFromGitHub {
     owner = "steipete";
     repo = "wacli";
     rev = "v${version}";
-    hash = "sha256-i7hZxLQkde4fzoFS7977gLFxoCsAVnmgdyqtBITHTxM=";
+    hash = "sha256-TP05tkE/bFFZbmOjLyAY2+cpqYwESFBG6Lz/rXhpSCA=";
   };
 in
-  pkgs.buildGoModule {
+  pkgs.buildGo127Module {
     inherit pname version src;
 
-    vendorHash = "sha256-Ymm/hn1xvMA9MnAtSQxMWoPDx5qNFv9bHR9D6031upI=";
+    vendorHash = "sha256-8Wo54XTj1tLshcAuiStmy+ux8R2tEHUaVSTCZQBMdnE=";
 
     subPackages = ["cmd/wacli"];
 
